da0542c176772dd78e453d5d5e76caaf1ae9837e
José Hernández <Jose_Hernandez_c@outlook.com>
2022-03-01 17:11:36 -0600

    dummy

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ludovic/main.dart';
+import 'package:ludovico_controller/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
